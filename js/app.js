311b28640ac6668dfb559823d73c1e6d6e833b2f
Matheus Cruvinel <matheuscruvinel@hotmail.com>
2025-03-21 21:33:00 -0300

    refactor: index.html frontend

--- js/app.js
+++ js/app.js
@@ -10,17 +10,10 @@ document.addEventListener('DOMContentLoaded', function () {
     let currentPage = 1;
     const itemsPerPage = 8;
 
-      // Add reference in the header
-      const header = document.createElement('div');
-      header.className = 'bg-blue-900 text-white py-8 text-center dark:bg-gray-800';
-      header.innerHTML = '<p class="text-sm text-white-700 dark:text-white-300">Desenvolvido por <a href="https://github.com/mcruvinel" class="text-blue-500 hover:underline" target="_blank">mcruvinel</a></p>';
-      document.body.insertBefore(header, document.body.firstChild);
-  
-      // Add reference in the footer
-      const footer = document.createElement('footer');
-      footer.className = 'bg-blue-900 text-white py-8 text-center dark:bg-gray-800';
-      footer.innerHTML = '<p class="text-sm text-white-700 dark:text-white-300">Código aberto disponível no <a href="https://github.com/mcruvinel" class="text-blue-500 hover:underline" target="_blank">GitHub</a></p>';
-      document.body.appendChild(footer);
+     // Criação do elemento de referência no header
+const header = document.createElement('div');
+document.body.insertBefore(header, document.body.firstChild);
+    document.body.insertBefore(header, document.body.firstChild);
 
     // Load data from JSON file
     fetch('data/churches.json')
